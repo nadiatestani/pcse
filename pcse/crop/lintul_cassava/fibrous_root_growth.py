c58a5770b631f5d54f37d6108957ac44a579cd3a
bergh026 <herman.berghuijs@wur.nl>
2026-02-26 13:59:14 +0100

    Fibrous_root_growth documented.

--- pcse/crop/lintul_cassava/fibrous_root_growth.py
+++ pcse/crop/lintul_cassava/fibrous_root_growth.py
@@ -1,7 +1,62 @@
+# -*- coding: utf-8 -*-
+# Herman Berghuijs (herman.berghuijs@wur.nl), Allard de Wit (allard.dewit@wur.nl), Tom Schut (tom.schut@wur.nl)
+# February 2026
+
 from pcse.base import ParamTemplate, RatesTemplate, SimulationObject, StatesTemplate
 from pcse.traitlets import Float
 
 class fibrous_root_growth(SimulationObject):
+    """
+    Class to simulate the rooting depth.
+
+    The class calculates the increase in rooting depth from emergence until a maximum rooting depth is reached.
+
+    ** Simulation parameters **
+
+    =================  ==============================================  ======  ===========================
+    Name               Description                                     Type     Unit
+    =================  ==============================================  ======  ===========================
+    RDI                Initial rooting depth                           SCr     cm root
+    RDMSOL             Maximum rooting depth                           SCr     cm root
+    RRDMAX             Maximum growth rate of rooting depth            SCr     cm root
+    SMW                Soil moisture content at permanent wilting
+                       point                                           SCr     cm root
+    =================  ==============================================  ======  ===========================
+
+    ** State variables **
+
+    =================  ==============================================  ======  ===========================
+    Name               Description                                     Pbl      Unit
+    =================  ==============================================  ======  ===========================
+    RD                 Rooting depth                                   Y       cm root
+    =================  ==============================================  ======  ===========================
+
+    ** Rate variables **
+
+    =================  ==============================================  ======  ===========================
+    Name               Description                                     Pbl      Unit
+    =================  ==============================================  ======  ===========================
+    RRD                Rate of change of rooting depth                 N        cm root
+    =================  ==============================================  ======  ===========================
+
+    This class is a Python implementation of the calculations related to the growth of rooting depth in the
+    R function LINTUL2_CASSAVA_NPK in the R version of the model LINTUL Cassava NPK (Adiele et al.,
+    2022; Ezui et al., 2018)
+
+    Authors LINTUL2_CASSAVA_NPK:     Rob van den Beuken, Joy Adiele, Tom Schut
+    Authors Python implementation:   Herman Berghuijs, Allard de Wit, Tom Schut
+
+    References:
+    Adiele J.G., Schut A.G.T., Ezui K.S., Giller K.E. (2022) LINTUL-Cassava-NPK: A simulation
+    model for nutrient-limited cassava growth. Field Crops Research 281: ARTN 108488
+
+    Ezui K.S., Leffelaar P.A., Franke A.C., Mando A., Giller K.E. (2018) Simulating drought impact
+    and mitigation in cassava using the LINTUL model. Field Crops Research 219: 256-272.
+    https://doi.org/10.1016/j.fcr.2018.01.033
+
+    """
+
+
 
     class Parameters(ParamTemplate):
         RDI = Float()
